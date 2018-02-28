f084ada1e55ef31003a07346432af22d47b49119
Julie <jca322@gmail.com>
2018-02-28 15:47:58 -0500

    Add basic shell for search-results component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatInputModule,
-         MatFormFieldModule, MatGridListModule } from '@angular/material';
+         MatFormFieldModule, MatGridListModule, MatDividerModule } from '@angular/material';
 import { RouterModule, Routes } from '@angular/router';
 
 
@@ -13,14 +13,15 @@ import { LoginComponent } from './components/login/login.component';
 import { CreateAccountComponent } from './components/create-account/create-account.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'create-account', component: CreateAccountComponent },
   { path: 'profile', component: ProfileComponent },
-  { path: 'edit-profile', component: EditProfileComponent }
-  // { path: '/home', component: }
+  { path: 'edit-profile', component: EditProfileComponent },
+  { path: 'search-results', component: SearchResultsComponent }
 ];
 
 @NgModule({
@@ -30,7 +31,8 @@ const routes: Routes = [
     LoginComponent,
     CreateAccountComponent,
     ProfileComponent,
-    EditProfileComponent
+    EditProfileComponent,
+    SearchResultsComponent
   ],
   imports: [
     BrowserModule,
@@ -44,7 +46,8 @@ const routes: Routes = [
     MatInputModule,
     MatFormFieldModule,
     RouterModule.forRoot(routes),
-    MatGridListModule
+    MatGridListModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
